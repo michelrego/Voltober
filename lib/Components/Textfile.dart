04bb93594590ba7cc2d5daf6f285370169a076aa
Michel Rego <43715929+michelrego@users.noreply.github.com>
2021-06-24 14:48:31 +0530

    Add files via upload

---
+++ lib/Components/Textfile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class Reusable extends StatelessWidget {
+  Reusable({  this.cardChild, this.onPress,this.label,this.labelMain,this.labelSub,this.labelSide});
+  final Widget cardChild;
+  final Function onPress;
+  final String label;
+  final String labelMain;
+  final String labelSub;
+  final String labelSide;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          label,
+          style: kEventTitleTextStyle,
+        ),
+        SizedBox(height: 10,),
+        Text(
+          labelMain,
+          style:kLabelTextStyle,
+        ),
+        SizedBox(height: 10,),
+        Text(
+          labelSub,
+          style:kLabelTextStyle,
+        ),
+        Divider(
+          height: 10,
+          color: Colors.black,
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(8,0,0,1),
+          child: Row(
+          children: <Widget>[
+              Text(
+                labelSide,
+                style:kLabelTextStyle,
+              ),
+              SizedBox(width: 70,),
+
+            MaterialButton(
+              color: Colors.purple[300],
+              onPressed: onPress,
+              child: Text('Apply'),
+            )
+          ],
+    ),
+        ),
+    ],
+    );
+  }
+}
---
+++ lib/Screens/Beachclean.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:volunteering/Components/Reusable_cards.dart';
+import 'package:volunteering/Components/Textfile.dart';
+import '../Components/widgets.dart';
+import '../constants.dart';
+import 'package:flutter/cupertino.dart';
+
+class Beach extends StatefulWidget {
+
+  @override
+  _BeachState createState() => _BeachState();
+}
+
+class _BeachState extends State<Beach> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        backgroundColor: kTitleColour,
+        title: Text('BEACH CLEANING', style: kDashTitleTextStyle),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  ReusableCard(
+                    colour: kCardColour,
+                    cardChild: Reusable(
+                      label: "Trash Free Sea",
+                      labelMain: 'Mangalore',
+                      labelSub: 'Internship| Unpaid| 1st July 21',
+                      labelSide: 'Apply by 29th June\'21 ',
+                      onPress: () {
+                        Navigator.pushNamed(context, '/output');
+                      },
+                    ),
+                  ),
+                  ReusableCard(
+                    colour:kCardColour,
+                    cardChild: Reusable(
+                      label: "Beach Cleansing",
+                      labelMain: 'Mangalore',
+                      labelSub: 'Internship| Unpaid| 1st July 21',
+                      labelSide: 'Apply by 29th June\'21 ',
+                      onPress: () {
+                        Navigator.pushNamed(context, '/output');
+                      },
+                    ),
+                  ),
+                  ReusableCard(
+                    colour: kCardColour,
+                    cardChild: Reusable(
+                      label: "Versova Beach Clean-up.",
+                      labelMain: 'Mangalore',
+                      labelSub: 'Internship| Unpaid| 1st July 21',
+                      labelSide: 'Apply by 29th June\'21 ',
+                      onPress: () {
+                        Navigator.pushNamed(context, '/output');
+                      },
+                    ),
+                  ),
+                  ReusableCard(
+                    colour: kCardColour,
+                    cardChild: Reusable(
+                      label: "Someshwar",
+                      labelMain: 'Mangalore',
+                      labelSub: 'Internship| Unpaid| 1st July 21',
+                      labelSide: 'Apply by 29th June\'21 ',
+                      onPress: () {
+                        Navigator.pushNamed(context, '/output');
+                      },
+                    ),
+                  ),
+                  ReusableCard(
+                    colour: kCardColour,
+                    cardChild: Reusable(
+                      label: "Sarvoham Foundation ",
+                      labelMain: 'Mangalore',
+                      labelSub: 'Internship| Unpaid| 1st July 21',
+                      labelSide: 'Apply by 29th June\'21 ',
+                      onPress: () {
+                        Navigator.pushNamed(context, '/output');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
