2ec3dd4b8948a13d089ee8175fa006e941ccde51
Michel Rego <43715929+michelrego@users.noreply.github.com>
2021-08-18 19:03:39 +0530

    Delete lib directory

--- lib/Controller/authentication.dart
+++
@@ -1,5 +0,0 @@
-import'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-FirebaseAuth auth = FirebaseAuth.instance;
-final googleSignIn = GoogleSignIn();
